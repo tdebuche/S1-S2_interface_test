b42eda820a19af445616b0cab88feb48a7e24082
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-12 16:41:58 +0200

    Create ts_to_pTTs.py

---
+++ S1_simulation/ts_to_pTTs.py
@@ -0,0 +1,44 @@
+import numpy as np
+import awkward as ak
+from data_handle.tools import getuvsector
+from collections import defaultdict
+
+
+
+def build_pTTsCEE(ts_energy, args, S1pTTCEE):
+    if args.Edges == 'yes': nb_phi = 28
+    else : nb_phi = 24
+    Sector = args.Sector
+    pTTsCEE = []
+    for pTT_idx in range(len(S1pTTCEE)):
+        energyCEE = 0
+        pTT_id = S1pTTCEE[pTT_idx]['pTT'] 
+        ModulesCEE = S1pTTCEE[pTT_idx]['Modules'] 
+        for module_idx in range(len(ModulesCEE)):
+            module_id = ModulesCEE[module_idx]['module_id']
+            energy = ModulesCEE[module_idx]['module_energy']
+            if ts_energy[module_id] != []:
+                energyCEE += ts_energy[module_id][0] * energy/16
+        pTTsCEE.append({'pTT_id' : pTT_id, 'energy': energyCEE})
+            #if energyCEE != 0:
+            #print({'pTT_id' : pTT_id, 'energy': energyCEE})
+    return(pTTsCEE)
+
+
+def build_pTTsCEH(stc_energy,args,S1pTTCEH):
+    if args.Edges == 'yes': nb_phi = 28
+    else : nb_phi = 24
+    Sector = args.Sector
+    pTTsCEH = []
+    for pTT_idx in range(len(S1pTTCEH)):
+        energyCEH = 0
+        pTT_id = S1pTTCEH[pTT_idx]['pTT'] 
+        STCsCEH = S1pTTCEH[pTT_idx]['Modules'] 
+        for stc_idx in range(len(STCsCEH)):
+            module_id = ModulesCEE[stc_idx]['stc_id']
+            stc = ModulesCEE[stc_idx]['module_idx']
+            energy = ModulesCEH[module_idx]['stc_energy']
+            if stc_energy[module_id] != []:
+                energyCEH += stc_energy[module_id][0][stc] * energy/16
+        pTTsCEE.append({'pTT_id' : pTT_id, 'energy': energyCEH})
+    return(pTTsCEH)
