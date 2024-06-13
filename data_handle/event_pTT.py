785c1f042f186c6596852702aaf6c65e8fbe95d1
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-13 10:59:45 +0200

    Update event_pTT.py

--- data_handle/event_pTT.py
+++ data_handle/event_pTT.py
@@ -13,6 +13,7 @@ class EventData():
         self.ds_si  = ds_si
         self.ds_sci  = ds_sci
         self.ds_ts = None
+        self.ds_unselected_ts = None
         self.ds_stc = None
         self.ds_pTTsCEE = None
         self.ds_pTTsdupCEE = None
@@ -25,7 +26,7 @@ class EventData():
         self.pT_gen  = self._compute_pt(self.eta_gen,
                              gen.good_genpart_energy[0])
 
-        self.data_packer = None
+        self.TCs_packer = None
         self.pTT_packer = None          ######################add##################
         self.LSB = 1/10000 # 100 keV
         self.LSB_r_z = 0.7/4096
@@ -38,41 +39,78 @@ class EventData():
     def ObjectType(self, object_type):
         return ((object_type & 0xF) << 22)
 
-    def provide_ts(self,args,xml):
-        nb_selected_TCs = defaultdict(list)
-        selected_TCs = self.ds_si
-        for module_idx in range(len(self.ds_si.good_tc_layer)):
-            u,v,sector = getuvsector(self.ds_si.good_tc_layer[module_idx][0],
-                                        self.ds_si.good_tc_waferu[module_idx][0],
-                                        self.ds_si.good_tc_waferv[module_idx][0])
-            module_alloc = self.get_module_id(3,self.ds_si.good_tc_layer[module_idx][0],u,v)
-            module = self.get_module_id(sector,self.ds_si.good_tc_layer[module_idx][0],u,v)
-            xml_alloc = self.get_TC_allocation(xml[0], module_alloc)
-            if xml_alloc: 
-                n_TCs = xml_alloc[-1]['index']
-                nb_selected_TCs[module].append(n_TCs)  
 
-        TCs = self.ds_si
-        ts = defaultdict(list)
-        Sector = args.Sector
-        for module_idx in range(len(self.ds_si.good_tc_layer)):
-            u,v,sector = getuvsector(self.ds_si.good_tc_layer[module_idx][0],
-                                        self.ds_si.good_tc_waferu[module_idx][0],
-                                        self.ds_si.good_tc_waferv[module_idx][0])
-            if u != -999:
-                module = self.get_module_id(sector,self.ds_si.good_tc_layer[module_idx][0],u,v)
-                if self.ds_si.good_tc_layer[module_idx][0] < 48:
-                    if ts[module] == []:
-                        ts[module].append(0)
-                    #for idx in range(len(self.ds_si.good_tc_layer[module_idx])):
-                    if nb_selected_TCs[module]:
-                        for idx in range(nb_selected_TCs[module][0],len(self.ds_si.good_tc_layer[module_idx])):
-                            ts[module][0] += self.ds_si.good_tc_pt[module_idx][idx]
-                    else : 
-                        print(sector, self.ds_si.good_tc_layer[module_idx][0],u,v)
-        
-        self.ds_ts = ts
-        return(nb_selected_TCs)
+def provide_event(ev, gen):
+    ev['r_over_z'] = np.sqrt(ev.good_tc_x**2 + ev.good_tc_y**2)/ev.good_tc_z
+    ev['MB_v'] = np.floor((ev.good_tc_cellv-1)/4)
+    #ev = ev[[x for x in ak.fields(ev) if not x in ["good_tc_x","good_tc_y","good_tc_z"]]]
+    
+    # dividing silicon and scintillators
+    sci = ev[ev['good_tc_subdet'] == 10]
+    si  = ev[ev['good_tc_subdet'] != 10]
+    
+    # selecting first 120 sector only
+    sci = sci[sci['good_tc_cellv']<=48]
+    #si = si[si['good_tc_layer'] < 27]
+
+    # sorting by modules  
+    sorted_waferu = si[ak.argsort(si['good_tc_waferu'])]
+    counts = ak.flatten(ak.run_lengths(sorted_waferu.good_tc_waferu), axis=None)
+    sorted_si = apply_sort(sorted_waferu, counts, 1)
+
+    sorted_waferv = sorted_si[ak.argsort(sorted_si['good_tc_waferv'])]
+    counts = ak.flatten(ak.run_lengths(sorted_waferv.good_tc_waferv), axis=None)
+    sorted_si = apply_sort(sorted_waferv, counts, 2)
+
+    sorted_layer = sorted_si[ak.argsort(sorted_si['good_tc_layer'])]
+    counts = ak.flatten(ak.run_lengths(sorted_layer.good_tc_layer), axis=None)
+    sorted_si = apply_sort(sorted_layer, counts, 3)
+    sorted_si = ak.flatten(sorted_si, axis=3)
+    sorted_si = ak.flatten(sorted_si, axis=2)
+
+    # sorting by transverse energy, simulating the ECONT_T
+    sorted_si = sorted_si[ak.argsort(sorted_si['good_tc_pt'], ascending=False)][0]
+    
+    # sorting sci by MB (cellv) and plane
+    sorted_MB = sci[ak.argsort(sci['MB_v'])]
+    counts = ak.flatten(ak.run_lengths(sorted_MB.MB_v), axis=None)
+    sorted_sci = apply_sort(sorted_MB, counts, 1)
+
+    sorted_layer = sorted_sci[ak.argsort(sorted_sci['good_tc_layer'])]
+    counts = ak.flatten(ak.run_lengths(sorted_layer.good_tc_layer), axis=None)
+    sorted_sci = apply_sort(sorted_layer, counts, 2)
+    sorted_sci = ak.flatten(sorted_sci, axis=2)
+
+    # sorting by transverse energy, simulating the ECONT_T
+    sorted_sci = sorted_sci[ak.argsort(sorted_sci['good_tc_pt'], ascending=False)][0]
+  
+    return EventData(sorted_si, sorted_sci,gen)
 
 
+
+def provide_events(n, particles, PU):
+    base_path = cfg_particles['base_path']
+    name_tree = cfg_particles[PU][particles]["tree"]
+    filepath  = base_path + cfg_particles[PU][particles]["file"]
+
+    branches_tc = [
+        'good_tc_x', 'good_tc_y', 'good_tc_z',
+        'good_tc_phi', 'good_tc_layer', 'good_tc_cellv',
+        'good_tc_waferu', 'good_tc_waferv',
+        'good_tc_pt', 'good_tc_subdet'
+    ]
+
+    branches_gen = [
+        'event', 'good_genpart_exeta', 'good_genpart_exphi', 'good_genpart_energy'
+    ]
+
+    tree = uproot.open(filepath)[name_tree]
+    events_ds = []
+    printProgressBar(0, n, prefix='Reading '+str(n)+' events from ROOT file:', suffix='Complete', length=50)
+    for ev in range(n):
+      data = tree.arrays(branches_tc, entry_start=ev, entry_stop=ev+1, library='ak')
+      data_gen = tree.arrays(branches_gen, entry_start=ev, entry_stop=ev+1, library='ak')[0]
+      events_ds.append(provide_event(data, data_gen))
+      printProgressBar(ev+1, n, prefix='Reading '+str(n)+' events from ROOT file:', suffix='Complete', length=50)
+    return events_ds
          
