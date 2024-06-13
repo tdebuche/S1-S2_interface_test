eb03355b2bc683f633adb93a069a3a4acdb563d2
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-13 16:04:04 +0200

    Update unpack_links.py

--- link_unpacking/unpack_links.py
+++ link_unpacking/unpack_links.py
@@ -55,6 +55,13 @@ def read_xml_plot(Edges,Sector):
         S1_index += 1
     return data_pTT
 
+def get_pTT_numbers(pTT):
+    S1Board = int(pTT[4:6],16) & 0x3F
+    phi = int(pTT,16) & 0x1F
+    eta = (int(pTT,16) & 0x3E0) //(16 * 2)
+    CEECEH = (int(pTT,16) & 0x400) //(16*16*4)
+    Sector = (int(pTT[2],16) &  0x6)//2
+    return(Sector,S1Board,eta,phi,CEECEH)
 
 def create_energies(data_links,etaphi_links,args):
     Edges = args.Edges
