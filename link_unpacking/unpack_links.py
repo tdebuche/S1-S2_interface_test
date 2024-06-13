2292a2bacd5435f367561f86c2e59682931b5a62
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-13 10:33:22 +0200

    Update unpack_links.py

--- link_unpacking/unpack_links.py
+++ link_unpacking/unpack_links.py
@@ -1,1 +1,108 @@
+import numpy as np
+import awkward as ak
+import uproot
+import math
+import yaml
+import os
+import argparse
+import numpy as np
+import xml.etree.ElementTree as ET
+from collections import defaultdict
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
+
+
+def read_xml_plot(Edges,Sector):
+    if Edges == 'yes':
+        tree = ET.parse('config_files/pTTs/Sector'+str(Sector)+'/AllocationpTTsEdges.xml')
+    if Edges == 'no':
+        tree = ET.parse('config_files/pTTs/Sector'+str(Sector)+'/AllocationpTTsNoEdges.xml')
+        
+    root = tree.getroot()
+    data_pTT  = defaultdict(list)
+
+    S1_index = 0
+    for s1_element in root.findall('.//S1'):
+        for channel_element in s1_element.findall('.//Channel'):
+            channel = int(channel_element.get('aux-id'))
+            for frame_element in channel_element.findall('.//Frame'):
+                if all(attr in frame_element.attrib for attr in ['id','pTT']):
+                    frame  = int(frame_element.get('id'))
+                    pTT     = frame_element.get('pTT')
+                    n_link = 14 + 14*math.floor(channel/2) + S1_index
+                    Sector,S1Board,eta,phi,CEECEH = get_pTT_numbers(pTT)
+                    data_pTT[(Sector,S1Board,eta,phi,CEECEH )].append((frame,n_link,channel%2))
+        S1_index += 1
+
+    
+    if Edges == 'yes':
+        tree = ET.parse('config_files/pTTs/Sector'+str(Sector)+'/DuplicationpTTsEdges.xml')
+    if Edges == 'no':
+        tree = ET.parse('config_files/pTTs/Sector'+str(Sector)+'/DuplicationpTTsNoEdges.xml')
+        
+    root = tree.getroot()
+
+    S1_index = 0
+    for s1_element in root.findall('.//S1'):
+        for channel_element in s1_element.findall('.//Channel'):
+            channel = int(channel_element.get('aux-id'))
+            for frame_element in channel_element.findall('.//Frame'):
+                if all(attr in frame_element.attrib for attr in ['id','pTT']):
+                    frame  = int(frame_element.get('id'))
+                    pTT     = frame_element.get('pTT')
+                    if channel//2 == 4:
+                        n_link =  S1_index
+                    if channel//2 == 5:
+                        n_link = 70 + S1_index
+                    Sector,S1Board,eta,phi,CEECEH = get_pTT_numbers(pTT)
+                    data_pTT[(Sector,S1Board,eta,phi,CEECEH )].append((frame,n_link,channel%2))
+                    
+
+        S1_index += 1
+    return data_pTT
+
+
+def create_energies(data_links,etaphi_links,args):
+    Edges = args.Edges
+    if Edges == 'yes': 
+        nb_phi = 28
+        offset = 3
+    else : 
+        nb_phi = 24
+        offset = 0
+        
+    Sector = args.Sector
+    energiesCEE = [[0 for phi in range(36)]for eta in range(20)]
+    for S1Board in range(14):
+        for eta in range(20):
+            for phi in range(36):
+                if etaphi_links[(Sector,S1Board,eta,phi+offset,0)] != []:
+                    if data_links[etaphi_links[(Sector,S1Board,eta,phi+offset,0)][0]] != []:
+                        energiesCEE[eta][phi] += data_links[etaphi_links[(Sector,S1Board,eta,phi+offset,0)][0]][0]
+                        #energies[eta][phi] += 1
+    for S1Board in range(14):
+        for eta in range(20):
+            for phi in range(36):
+                if etaphi_links[(Sector+1,S1Board,eta,phi-24+offset,0)] != []:
+                    if data_links[etaphi_links[(Sector+1,S1Board,eta,phi-24+offset,0)][0]] != []:
+                        energiesCEE[eta][phi] += data_links[etaphi_links[(Sector+1,S1Board,eta,phi-24+offset,0)][0]][0]
+                        #energies[eta][phi] += 1
+    
+    energiesCEH = [[0 for phi in range(36)]for eta in range(20)]
+    for S1Board in range(14):
+        for eta in range(20):
+            for phi in range(36):
+                if etaphi_links[(Sector,S1Board,eta,phi+offset,1)] != []:
+                    if data_links[etaphi_links[(Sector,S1Board,eta,phi+offset,1)][0]] != []:
+                        energiesCEH[eta][phi] += data_links[etaphi_links[(Sector,S1Board,eta,phi+offset,1)][0]][0]
+                        #energies[eta][phi] += 1
+    for S1Board in range(14):
+        for eta in range(20):
+            for phi in range(36):
+                if etaphi_links[(Sector+1,S1Board,eta,phi-24+offset,1)] != []:
+                    if data_links[etaphi_links[(Sector+1,S1Board,eta,phi-24+offset,1)][0]] != []:
+                        energiesCEH[eta][phi] += data_links[etaphi_links[(Sector+1,S1Board,eta,phi-24+offset,1)][0]][0]
+                        #energies[eta][phi] += 1
+    return(energiesCEE,energiesCEH)
