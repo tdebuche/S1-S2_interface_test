0a6da750ed43e65a090686c0651d043337cd1462
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-13 16:46:49 +0200

    Update packer_pTTs.py

--- pTTs_mapping/packer_pTTs.py
+++ pTTs_mapping/packer_pTTs.py
@@ -1,73 +1,39 @@
 from collections import defaultdict
 import numpy as np
-import awkward as ak
-import uproot
 import math
-import yaml
+from data_handle.tools import getuvsector,get_module_id,get_pTT_id
+from pTTs_mapping.read_files import read_allocation_pTTs
 
 
-def _process_eventpTT(self,args, xml_allocation,xml_duplication,S1pTTCEE,S1pTTCEH,S1pTTCEEdup,S1pTTCEHdup,xml):
-        data_pTTs = defaultdict(list)
-        Sector = args.Sector
-        nb_selected_TCs = self.provide_ts(args,xml)
 
-        #CEE
+def fill_links(pTTs,links,xml):
+    for pTT_idx in range(len(pTTs)):
+        pTT = pTTs[pTT_idx]['pTT_id']
+        pTT_xml = xml[pTT]
+        if pTT_xml != [] :    #if pTTs is allocated in the 4 links
+            if links[(pTT_xml[0]['frame'], pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)] == []:
+                links[(pTT_xml[0]['frame'],pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)].append(pTTs[pTT_idx]['energy'])
+            else: print('error link is already filled')
+        else : print('error pTT has no allocation')
+            
 
-        #Sector 0
-        self.ds_pTTsCEE = build_pTTsCEE(self.ds_ts, args, S1pTTCEE)  #from module sums
-        self.ds_pTTsCEE  = add_TCs(self.ds_pTTsCEE,self.ds_si,nb_selected_TCs,0,'CEE') #add selected TCs
-        pTTs = self.ds_pTTsCEE
+def _process_eventpTT(event,args,xml_allocation,xml_duplication):
+    data_pTTs = defaultdict(list)
     
+    #fill CEE links 
+    fill_links(event.ds_pTTsCEE,data_pTTs,xml_allocation)
+    fill_links(event.ds_pTTsdupCEE,data_pTTs,xml_duplication)
 
-        #Sector 1
-        self.ds_pTTsdupCEE = build_pTTsCEE(self.ds_ts, args, S1pTTCEEdup)
-        self.ds_pTTsdupCEE  = add_TCs(self.ds_pTTsdupCEE,self.ds_si,nb_selected_TCs,1,'CEE')
-        pTTsdup = self.ds_pTTsdupCEE
-
-        #fill CEE links 
-        
-        for pTT_idx in range(len(pTTs)):
-            pTT = pTTs[pTT_idx]['pTT_id']
-            pTT_xml = self.get_pTT_allocation(xml_allocation, pTT)
-            if pTT_xml != [] :    #if pTT is allocated in the 4 links
-                data_pTTs[(pTT_xml[0]['frame'],pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)].append(pTTs[pTT_idx]['energy'])
-        for pTT_idx in range(len(pTTsdup)):
-            pTT = pTTsdup[pTT_idx]['pTT_id']
-            pTT_xml = self.get_pTT_duplication(xml_duplication, pTT)
-            if pTT_xml != [] :    #if pTT is allocated in the 2 links
-                if data_pTTs[(pTT_xml[0]['frame'], pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)] == []:
-                    data_pTTs[(pTT_xml[0]['frame'], pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)].append(pTTsdup[pTT_idx]['energy'])
+    #fill CEH links
+    fill_links(event.ds_pTTsCEE,data_pTTs,xml_allocation)
+    fill_links(event.ds_pTTsdupCEE,data_pTTs,xml_duplication)
+    
+    return data_pTTs
 
 
-        #CEH
 
-        #Sector 0 
-        self.ds_pTTsCEH = build_pTTsCEE(self.ds_ts, args, S1pTTCEH)
-        self.ds_pTTsCEH  = add_TCs(self.ds_pTTsCEH,self.ds_si,nb_selected_TCs,0,'CEH')
-        pTTs = self.ds_pTTsCEH
-
-        #Sector 1
-        self.ds_pTTsdupCEH = build_pTTsCEE(self.ds_ts, args, S1pTTCEHdup)
-        self.ds_pTTsdupCEH  = add_TCs(self.ds_pTTsdupCEH,self.ds_si,nb_selected_TCs,1,'CEH')
-        pTTsdup = self.ds_pTTsdupCEH
-
-        #fill CEH links
-        
-        for pTT_idx in range(len(pTTs)):
-            pTT = pTTs[pTT_idx]['pTT_id']
-            pTT_xml = self.get_pTT_allocation(xml_allocation, pTT)
-            if pTT_xml != [] :    #if pTT is allocated in the 4 links
-                data_pTTs[(pTT_xml[0]['frame'],pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)].append(pTTs[pTT_idx]['energy'])
-        for pTT_idx in range(len(pTTsdup)):
-            pTT = pTTsdup[pTT_idx]['pTT_id']
-            pTT_xml = self.get_pTT_duplication(xml_duplication, pTT)
-            if pTT_xml != [] :    #if pTT is allocated in the 2 links
-                if data_pTTs[(pTT_xml[0]['frame'], pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)] == []:
-                    data_pTTs[(pTT_xml[0]['frame'], pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)].append(pTTsdup[pTT_idx]['energy'])
-
-        return data_pTTs
-
-
-    def _pTT_packer(self, args, xml_allocation,xml_duplication,S1pTTCEE,S1pTTCEH,S1pTTCEEdup,S1pTTCEHdup,xml):
-        data_pTTs = self._process_eventpTT(args, xml_allocation,xml_duplication,S1pTTCEE,S1pTTCEH,S1pTTCEEdup,S1pTTCEHdup,xml)
-        self.pTT_packer =  data_pTTs
+def _pTT_packer(event, args):
+    xml_allocation = read_allocation_pTTs(args.Edges,args.Sector,4)
+    xml_duplication = read_allocation_pTTs(args.Edges,args.Sector,2)
+    data_pTTs = _process_eventpTT(event,args,xml_allocation,xml_duplication)
+    event.pTT_packer =  data_pTTs
