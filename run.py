c5e098b4c94a4ff8378eec22abde8f27420daadc
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-06-27 11:44:09 +0200

    Update run.py

--- run.py
+++ run.py
@@ -13,12 +13,19 @@ from link_packing.packer_TCs import _TC_packer
 from link_packing.packer_pTTs import _pTT_packer
 from link_unpacking.plot_TTs import record_plot
 
-parser = argparse.ArgumentParser(description='Stage-2 Emulator Parameters')
+parser = argparse.ArgumentParser(description='S1-S2_interface_test Parameters')
+
+#version to test
+parser.add_argument('--pTT_version', default='v3', help='pTT version to test')
+
+#event type
 parser.add_argument('-n',          type=int, default=1,         help='Provide the number of events')
 parser.add_argument('--particles', type=str, default='photons', help='Choose the particle sample')
-parser.add_argument('--Scenario', type=str, default='TS', help='Choose TS or unselected_TS')
 parser.add_argument('--pileup',    type=str, default='PU0',     help='Choose the pileup - PU0 or PU200')
-parser.add_argument('--Sector',      type=int, default=0, help='Sector of S2 Board')
+
+#On scenarios 
+parser.add_argument('--Scenario', type=str, default='TS', help='Choose TS or unselected_TS')
+parser.add_argument('--Sector',      type=int, default=0, help='Sector of S2 Board, only the sector 0 works')
 parser.add_argument('--Edges',   default = 'yes', help='20*24 or 20*28 bins')
 
 args = parser.parse_args()
