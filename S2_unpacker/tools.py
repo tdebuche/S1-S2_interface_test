9a5e953ee38ad53b2826073a0b63a6504ac85825
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-03 13:38:51 +0200

    Create tools.py

---
+++ S2_unpacker/tools.py
@@ -0,0 +1,7 @@
+def get_pTT_numbers(pTT):
+    S1Board = int(pTT[4:6],16) & 0x3F
+    phi = int(pTT,16) & 0x1F
+    eta = (int(pTT,16) & 0x3E0) //(16 * 2)
+    CEECEH = (int(pTT,16) & 0x400) //(16*16*4)
+    Sector = (int(pTT[2],16) &  0x6)//2
+    return(Sector,S1Board,eta,phi,CEECEH)
