3cca5488d6506b4e9b9dfa036f64e5657982e1e5
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-03 16:07:00 +0200

    Update read_EMP_file.py

--- S2_unpacker/read_EMP_file.py
+++ S2_unpacker/read_EMP_file.py
@@ -106,7 +106,6 @@ def get_pTTs_from_EMPfile(args,EMPfile,pTT_allocation,TC_allocation):
                 print('wrong header')
             word = frame_element[2 + n_link *2 + 1]
             for pTT_number in range(2):
-                print(word,pTT_number)
                 pTT_energy = get_pTT_energy(word,pTT_number)
                 if pTT_allocation[(frame_index,n_link,pTT_number)]:
                     Sector,S1Board,eta,phi,CEECEH = pTT_allocation[(frame_index,n_link,pTT_number)][0]
