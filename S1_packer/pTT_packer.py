0befc40e19bd798fdd820b5b186cd4c32e15b8d6
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-03 13:23:44 +0200

    Update pTT_packer.py

--- S1_packer/pTT_packer.py
+++ S1_packer/pTT_packer.py
@@ -13,7 +13,7 @@ def fill_links(pTTs,links,xml):
             if links[(pTT_xml[0]['frame'], pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)] == []:
                 links[(pTT_xml[0]['frame'],pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)].append(pTTs[pTT_idx]['energy'])
             else: 
-                [print('error link is already filled',(pTT_xml[0]['frame'],pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2), links[(pTT_xml[0]['frame'], pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)])
+                print('error link is already filled',(pTT_xml[0]['frame'],pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2), links[(pTT_xml[0]['frame'], pTT_xml[0]['n_link'],pTT_xml[0]['channel']%2)])
         #else : print('error pTT has no allocation')
     #print(links)
             
