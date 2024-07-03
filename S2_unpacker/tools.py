ca7cd523b3d0071a1eb25701b92b415bd2f773cb
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-03 15:34:00 +0200

    Update tools.py

--- S2_unpacker/tools.py
+++ S2_unpacker/tools.py
@@ -10,4 +10,30 @@ def get_pTT_numbers(pTT):
 
 
 def get_pTT_energy(word,pTT_number):
+    if pTT_number == 1:
+        coded_energy = int(word[0:3],16) & 0x1fe
+    if pTT_number == 1:
+        coded_energy = int(word[2:5],16) & 0x1fe
+    energy_in_integer = unpack5E3M_ToInt(coded_energy)
+    energy_in_GeV = unpackFloat_FromInt(energy_in_integer)
     
+
+
+def unpack5E3M_ToInt(num): #Take 5E3M format and unpackpack it into integer
+    assert(num<0x100);#make sure input is an 8 bit number
+
+    e = ((num>>3)&0x1f);#Read first 5 bits from the input number to read the exponent
+    m = ((num   )&0x07);#Read the last 3 bits as mantissa
+    #print(e,m)
+
+    if(e==0):
+        return m
+    elif(e==1):
+        return 8+m
+    else:
+        return (16+2*m+1)<<(e-2)
+
+
+
+def unpackFloat_FromInt(energy):
+    return float((energy-0.5)/(2**12))
