97f6fc51108a5440dbdd3514e4de9fc9319db9a3
tdebuche <166105894+tdebuche@users.noreply.github.com>
2024-07-05 10:21:41 +0200

    Update read_EMP_file.py

--- S2_unpacker/read_EMP_file.py
+++ S2_unpacker/read_EMP_file.py
@@ -34,7 +34,7 @@ def read_allocation(Edges,Sector):
     if Edges == 'yes':
         tree = ET.parse('config_files/pTT_allocation/28_phi_bins/Sector'+str(Sector)+'/Allocation.xml')
     if Edges == 'no':
-        tree = ET.parse(''config_files/pTT_allocation/24_phi_bins/Sector'+str(Sector)+'/Duplication.xml')
+        tree = ET.parse('config_files/pTT_allocation/24_phi_bins/Sector'+str(Sector)+'/Duplication.xml')
         
     root = tree.getroot()
 
